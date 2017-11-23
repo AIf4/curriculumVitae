09b44c3de79664a4ec60d5b376ebd183d6eaf232
Alf4 <rutbak@gmail.com>
2017-11-23 17:52:18 -0500

    Adelantando la estructura de la pagina, enlaces funcionando

--- js/master.js
+++ js/master.js
@@ -1,6 +1,51 @@
 // general functions
 $(function() {
-    $("#profile").hide();
+    // $("#profile").hide();
     $("#academy").hide();
+    $("#skill").hide();
+    $("#laboral").hide();
+    $("#contact").hide();
 
+
+
+    // eventos
+    $("#profileButton").click(function(){
+        $("#academy").hide();
+        $("#profile").show();
+        $("#skill").hide();
+        $("#laboral").hide();
+        $("#contact").hide();
+    });
+
+    $("#academyButton").click(function(){
+        $("#academy").toggle();
+        $("#profile").hide();
+        $("#skill").hide();
+        $("#laboral").hide();
+        $("#contact").hide();
+    });
+
+    $("#skillButton").click(function(){
+        $("#academy").hide();
+        $("#profile").hide();
+        $("#skill").toggle();
+        $("#laboral").hide();
+        $("#contact").hide();
+    });
+
+    $("#laboralButton").click(function(){
+        $("#academy").hide();
+        $("#profile").hide();
+        $("#skill").hide();
+        $("#laboral").toggle();
+        $("#contact").hide();
+    });
+
+    $("#contactButton").click(function(){
+        $("#academy").hide();
+        $("#profile").hide();
+        $("#skill").hide();
+        $("#laboral").hide();
+        $("#contact").toggle();
+    });
 });
